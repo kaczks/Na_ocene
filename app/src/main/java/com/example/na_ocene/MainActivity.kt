22baa6669db00d2c5b50c7915412311f561a296d
kaczks <M.Golas@student.zschie.edu.pl>
2021-10-29 15:10:07 +0200

    na wszelki

--- app/src/main/java/com/example/na_ocene/MainActivity.kt
+++ app/src/main/java/com/example/na_ocene/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.na_ocene
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var CheckBox = findViewById<CheckBox>(R.id.checkBox)
+        var
+
     }
+
 }
