afd619fa659d7e9f3ec9c87175e454c725384843
kaczks <M.Golas@student.zschie.edu.pl>
2021-10-29 15:52:06 +0200

    dodanie unkcji nastepny i poprzedni w galerii czyszczenie obrazka

--- app/src/main/java/com/example/na_ocene/MainActivity.kt
+++ app/src/main/java/com/example/na_ocene/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
 
         var CheckBox = findViewById<CheckBox>(R.id.checkBox)
         var ImageView = findViewById<ImageView>(R.id.Obraz)
+        var ImageButton_next = findViewById<ImageButton>(R.id.Nastepny)
+        var ImageButton_poprzedni = findViewById<ImageButton>(R.id.Poprzedni)
+        var ImageButton_wyczysc = findViewById<ImageButton>(R.id.wyczyść)
+
         CheckBox.setOnClickListener() {
             if (ImageView.getVisibility() == View.INVISIBLE) {
                 ImageView.setVisibility(View.VISIBLE); }
@@ -21,6 +25,53 @@ class MainActivity : AppCompatActivity() {
                 ImageView.setVisibility(View.INVISIBLE);
             }
 
+        }
+        var image= "one"
+        ImageButton_next.setOnClickListener{
+            when(image){
+                "one" -> {
+                    ImageView.setImageResource(R.drawable.czarny)
+                    image = "two"
+                }
+                "two" -> {
+                    ImageView.setImageResource(R.drawable.czerwony)
+                    image = "three"
+                }
+                "three" -> {
+                    ImageView.setImageResource(R.drawable.niebieski)
+                    image = "four"
+                }
+                "four" -> {
+                    ImageView.setImageResource(R.drawable.zielony)
+                    image = "one"
+                }
+            }
+
+        }
+        ImageButton_poprzedni.setOnClickListener{
+            when(image){
+                "one" -> {
+                    ImageView.setImageResource(R.drawable.niebieski)
+                    image = "four"
+                }
+                "four" -> {
+                    ImageView.setImageResource(R.drawable.czerwony)
+                    image = "three"
+                }
+                "three" -> {
+                    ImageView.setImageResource(R.drawable.czarny)
+                    image = "two"
+                }
+                "two" -> {
+                    ImageView.setImageResource(R.drawable.zielony)
+                    image = "one"
+                }
+            }
+
+        }
+
+        ImageButton_wyczysc.setOnClickListener{
+            ImageView.setImageResource(R.drawable.ic_launcher_background)
         }
 
     }
