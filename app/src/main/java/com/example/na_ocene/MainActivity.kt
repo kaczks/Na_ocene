c1b0ca6817c29e0aa500129fb8815346b5336711
Kaczks <M.Golas@student.zschie.edu.pl>
2021-11-01 21:43:10 +0100

    Dadawanie funkcji robienia zdjecia

--- app/src/main/java/com/example/na_ocene/MainActivity.kt
+++ app/src/main/java/com/example/na_ocene/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.na_ocene
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.CheckBox
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,18 +20,20 @@ class MainActivity : AppCompatActivity() {
         var ImageButton_next = findViewById<ImageButton>(R.id.Nastepny)
         var ImageButton_poprzedni = findViewById<ImageButton>(R.id.Poprzedni)
         var ImageButton_wyczysc = findViewById<ImageButton>(R.id.wyczyść)
+        var ImageButton_kamera = findViewById<ImageButton>(R.id.Kamera)
 
         CheckBox.setOnClickListener() {
             if (ImageView.getVisibility() == View.INVISIBLE) {
-                ImageView.setVisibility(View.VISIBLE); }
-            else{
+                ImageView.setVisibility(View.VISIBLE); } else {
                 ImageView.setVisibility(View.INVISIBLE);
             }
 
         }
-        var image= "one"
-        ImageButton_next.setOnClickListener{
-            when(image){
+
+
+        var image = "one"
+        ImageButton_next.setOnClickListener {
+            when (image) {
                 "one" -> {
                     ImageView.setImageResource(R.drawable.czarny)
                     image = "two"
@@ -48,8 +53,8 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        ImageButton_poprzedni.setOnClickListener{
-            when(image){
+        ImageButton_poprzedni.setOnClickListener {
+            when (image) {
                 "one" -> {
                     ImageView.setImageResource(R.drawable.niebieski)
                     image = "four"
@@ -70,10 +75,35 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        ImageButton_wyczysc.setOnClickListener{
+        ImageButton_wyczysc.setOnClickListener {
             ImageView.setImageResource(R.drawable.ic_launcher_background)
+        }
+        ImageButton_kamera.isEnabled = false
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 111)
+        } else {
+            ImageButton_kamera.isEnabled = true
         }
 
     }
-}
+
+
+        override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+        ) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            findViewById<ImageButton>(R.id.Kamera).isEnabled=true
+            }
+        }
+    }
+
 
